ee428ca4578a7a9fd61043e5adbd025aeacdc96c
kirankumarkarmala1996 <108362315+kirankumarkarmala1996@users.noreply.github.com>
2023-03-14 11:17:14 +0530

    Latest changes-1
    Latest Changes-1

---
+++ src/main/java/reports/TestNGAnnotation.java
@@ -0,0 +1,11 @@
+package reports;
+
+public class TestNGAnnotation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Automation testing");
+
+	}
+
+}
