5b142743ad280250af159f6b7ea218621ea36db9
kirankumarkarmala1996 <108362315+kirankumarkarmala1996@users.noreply.github.com>
2023-03-16 13:45:41 +0530

    changes done

--- src/main/java/com/MouseOverAction/Hello.java
+++
@@ -1,5 +0,0 @@
-package com.MouseOverAction;
-
-public class Hello {
-
-}
--- src/main/java/com/MouseOverAction/Testing.java
+++ src/main/java/com/MouseOverAction/Testing.java
@@ -2,7 +2,32 @@ package com.MouseOverAction;
 
 public class Testing {
 	public static void main(String[] args) {
-		System.out.println("Testing");
+		
+		int []arr= {1,2,3};
+		int []arr1= {1,2,4};
+		int []arr2 = {2,2,4,5};
+		
+		
+		// print the common number in this three arrays
+		/*for (int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr1.length;j++) {
+				for(int k=0;k<arr2.length;k++)
+				if(arr[i]==arr1[j] && arr[i]==arr2[k])
+					System.out.println(arr1[j]);
+				
+			}
+		}
+		*/
+		
+		//to find out the duplicates in two arrays
+		for (int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr1.length;j++) {
+				
+				if(arr[i]==arr1[j])
+					System.out.println(arr1[j]);
+				
+			}
+		}
 	}
 
 }
